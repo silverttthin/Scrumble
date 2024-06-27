3683726c395f78d23307a964b3bb29fa2fee55e7
leesw <silverttthin@naver.com>
2024-06-27 21:10:04 +0900

    feat: Phone contacts connected!!

--- lib/tab1.dart
+++ lib/tab1.dart
@@ -1,34 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class tab1 extends StatefulWidget {
   const tab1({super.key});
-
   @override
   State<tab1> createState() => _tab1State();
 }
 
 class _tab1State extends State<tab1> {
+  List<Contact>? _contacts;
 
-  List<Widget> contact = [
-    Container(
-      height: 50,
-      child: Center(child: Text('010-1234-5678'),),
-    ),
-    Container(
-      height: 50,
-      child: Center(child: Text('010-6490-2661'),),
-    ),
-    Container(
-      height: 50,
-      child: Center(child: Text('010-1232-1111'),),
-    ),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    getContacts();
+  }
+
+  Future<void> getContacts() async{
+    if(await Permission.contacts.request().isGranted){
+      print("permission is granted!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+      var contacts = await ContactsService.getContacts(
+        withThumbnails: false,
+      );
+
+      // var -> state
+      setState(() {
+        _contacts = contacts;
+      });
+    }
+    else{
+      print("contact is rejected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: contact,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('연락처'),
+      ),
+      body: _contacts == null
+          ? Center(child: Text("_contacts is null.."))
+          : _contacts!.isEmpty
+          ? Center(child: Text('No contacts found'))
+          : ListView.builder(
+        itemCount: _contacts!.length,
+        itemBuilder: (context, index) {
+          Contact contact = _contacts![index];
+          return ListTile(
+            title: Text(contact.displayName ?? 'No Name'),
+            subtitle: Text(contact.phones!.isNotEmpty ? contact.phones!.first.value ?? 'No Number' : 'No Number'),
+          );
+        },
+      ),
     );
   }
 }
