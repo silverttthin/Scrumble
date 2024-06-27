0564504975ccf90bbd85e409eca1d19dbd0248d4
leesw <silverttthin@naver.com>
2024-06-27 15:38:37 +0900

    feat: initial structure

---
+++ lib/tab2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tab2 extends StatefulWidget {
+  const tab2({super.key});
+
+  @override
+  State<tab2> createState() => _tab2State();
+}
+
+class _tab2State extends State<tab2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
