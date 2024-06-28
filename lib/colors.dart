bc309ad6976b0d95db1cd1d8bd991b9121a63872
edge <j2hyeon02@kaist.ac.kr>
2024-06-28 15:54:25 +0900

    feat: change color

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class MadColor {
   static const mainColor = Color(0xFF8E45FF);
-  static const backgroudColor = Color(0xFFf6f0ff);
-  static const mainLight = Color(0xffd7baff);
+  static const backgroudColor = Color(0xFFF6F0FF);
+  static const mainLight = Color(0xffFAF7FF);
 }
