7c7ce07bf05bd5d0aa9202732a654bae4cf60c46
edge <j2hyeon02@kaist.ac.kr>
2024-06-28 21:32:27 +0900

    feat: change background

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: HomePage(),
-
+      theme: ThemeData(scaffoldBackgroundColor: MadColor.backgroudColor),
     );
   }
 }
