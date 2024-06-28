7a8dfbe223742475e1ecc5007f77607a9f722678
edge <j2hyeon02@kaist.ac.kr>
2024-06-28 13:50:19 +0900

    feat: add color

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MadColor {
+  static const mainColor = Color(0xFF8E45FF);
+  static const backgroudColor = Color(0xFFf6f0ff);
+  static const mainLight = Color(0xffd7baff);
+}
