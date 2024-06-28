4e7df3af45c971631e435dc709a41dd48379f820
edge <j2hyeon02@kaist.ac.kr>
2024-06-28 15:55:16 +0900

    feat: add scrum add page

---
+++ lib/scrum_add_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScrumAddPage extends StatefulWidget {
+  const ScrumAddPage({super.key});
+
+  @override
+  State<ScrumAddPage> createState() => _ScrumAddPageState();
+}
+
+class _ScrumAddPageState extends State<ScrumAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
