997bf715f74a90798d1dea5de97f107ca8feef21
edge <j2hyeon02@kaist.ac.kr>
2024-06-29 11:59:28 +0900

    feat

---
+++ lib/page/scrum_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScrumPage extends StatefulWidget {
+  const ScrumPage({super.key});
+
+  @override
+  State<ScrumPage> createState() => _ScrumPageState();
+}
+
+class _ScrumPageState extends State<ScrumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
