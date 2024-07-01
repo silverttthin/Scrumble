8bf7c8189c30f61c1f4aff9c9bb153eb3142dffb
edge <j2hyeon02@kaist.ac.kr>
2024-07-01 17:45:38 +0900

    feat: change tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:madcamp_week1_mission/Model/human_provider.dart';
@@ -11,6 +13,9 @@ import 'package:madcamp_week1_mission/page/scrum_page.dart';
 import 'package:madcamp_week1_mission/page/feed_page.dart';
 import 'package:madcamp_week1_mission/page/tab2.dart';
 import 'package:madcamp_week1_mission/page/tab2_page.dart';
+import 'package:motion_tab_bar/MotionBadgeWidget.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:motion_tab_bar/MotionTabBarController.dart';
 import 'package:provider/provider.dart';
 
 void main() async{
@@ -53,39 +58,111 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  List _pages = [
-    ScrumAddPage(),
-    Tab2Page(),
-  ];
-  
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  MotionTabBarController? _motionTabBarController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //// Use normal tab controller
+    // _tabController = TabController(
+    //   initialIndex: 1,
+    //   length: 4,
+    //   vsync: this,
+    // );
+
+    //// use "MotionTabBarController" to replace with "TabController", if you need to programmatically change the tab
+    _motionTabBarController = MotionTabBarController(
+      initialIndex: 1,
+      length: 3,
+      vsync: this,
+    );
+
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final humanData = Provider.of<HumanModel>(context, listen: false);
+      humanData.getHuman();
+      humanData.getImage();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // _tabController.dispose();
+    _motionTabBarController!.dispose();
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    final humanData = Provider.of<HumanModel>(context);
-    humanData.getHuman();
+    // final humanData = Provider.of<HumanModel>(context);
+    // humanData.getHuman();
 
     return Scaffold(
-      body: _pages[_selectedIndex],
+      // body: _pages[_selectedIndex],
       appBar: AppBar(
         title: Text('SCRUMBLE'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blueAccent,
-        selectedFontSize: 14.0,
-        unselectedFontSize: 12.0,
-        enableFeedback: false,
-        onTap: (int index){
-          setState((){
-            _selectedIndex = index;
+      bottomNavigationBar: MotionTabBar(
+        controller: _motionTabBarController, // ADD THIS if you need to change your tab programmatically
+        initialSelectedTab: "피드",
+        labels: const ["추가", "피드", "개요"],
+        icons: const [Icons.add_outlined, Icons.dashboard_outlined, Icons.analytics_outlined],
+
+        // optional badges, length must be same with labels
+
+        tabSize: 45,
+        tabBarHeight: 50,
+        textStyle: const TextStyle(
+          fontSize: 12,
+          color: MadColor.mainColor,
+          fontWeight: FontWeight.w800,
+        ),
+        tabIconColor: MadColor.mainColor,
+        tabIconSize: 28.0,
+        tabIconSelectedSize: 26.0,
+        tabSelectedColor: MadColor.mainColor,
+        tabIconSelectedColor: Colors.white,
+        tabBarColor: Colors.white,
+        onTabItemSelected: (int value) {
+          setState(() {
+            // _tabController!.index = value;
+            _motionTabBarController!.index = value;
           });
         },
-        currentIndex: _selectedIndex,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.call_outlined), label: '연락처'),
-          BottomNavigationBarItem(icon: Icon(Icons.photo_library_outlined), label: '갤러리'),
+      ),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(), // swipe navigation handling is not supported
+        // controller: _tabController,
+        controller: _motionTabBarController,
+        children: [
+          ScrumAddPage(),
+          FeedPage(),
+          const Center(
+            child: Text("개요"),
+          ),
         ],
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   selectedItemColor: Colors.blueAccent,
+      //   selectedFontSize: 14.0,
+      //   unselectedFontSize: 12.0,
+      //   enableFeedback: false,
+      //   onTap: (int index){
+      //     setState((){
+      //       _selectedIndex = index;
+      //     });
+      //   },
+      //   currentIndex: _selectedIndex,
+      //   items: [
+      //     BottomNavigationBarItem(icon: Icon(Icons.call_outlined), label: '연락처'),
+      //     BottomNavigationBarItem(icon: Icon(Icons.photo_library_outlined), label: '갤러리'),
+      //   ],
+      // ),
     );
   }
 
